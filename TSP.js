f4668a7ba8ba54d034ce23aaece2a3c05c30739b
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-11-13 22:23:58 +0000

    fin

--- TSP.js
+++ TSP.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const prompt = require('prompt-sync')();
-
-const runDijkstra = (start, end, map) => {
-    
-}
+const PriorityQueue = require('js-priority-queue'); 
 
 const fileContents = fs.readFileSync('input.txt', 'utf8');
 
@@ -24,8 +21,51 @@ for (var l = 1; l < fileLines.length; l++) {
     map[from][to] = weight;
     map[to][from] = weight;
 }
-console.log(map);
 
-const start = prompt('Start?');
-const end = prompt('End?');
+const start = prompt('Start: ');
+const end = prompt('End: ');
+const nodeDistances = new Array(nodeCount).fill({from: Infinity, weight: Infinity});
+const visited = new Array(nodeCount).fill(false);
 
+const runDijkstra = (startNode) => {
+    nodeDistances[startNode] = { from: null, weight: 0 };
+
+    var queue = new PriorityQueue({ comparator: (a, b) => a.weight - b.weight });
+    queue.queue({ nodeNum: startNode, weight: 0 });
+
+    while (queue.length > 0) {
+        var currentNode = queue.dequeue().nodeNum;
+
+        if (visited[currentNode]) {
+            continue;
+        }
+
+        visited[currentNode] = true;
+
+        map[currentNode].forEach((weight, neighbor) => {
+            if (weight !== 0 && (!nodeDistances[neighbor] || nodeDistances[neighbor].weight > nodeDistances[currentNode].weight + weight)) {
+                nodeDistances[neighbor] = { from: currentNode, weight: nodeDistances[currentNode].weight + weight };
+                queue.queue({ nodeNum: neighbor, weight: nodeDistances[neighbor].weight });
+            }
+        });
+    }
+}
+
+runDijkstra(start);
+
+console.log("The shortest path from " + start + " to " + end + " is the following:");
+
+var from = end;
+var path = [];
+while (from !== null){
+    path.push(from);
+    from = nodeDistances[from].from;
+}
+
+for(var i = path.length - 1; i >= 0; i--){
+    process.stdout.write(`${path[i]}`);
+    if(i !== 0)
+        process.stdout.write(' --> ');
+}
+
+console.log(" with a weight of " + nodeDistances[end].weight);
